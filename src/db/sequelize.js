9fa0aaa46f6eb637e44afe0a9368fb83998aedc3
HamzaJsr <gestiondev.j@gmail.com>
2024-05-09 16:42:28 +0200

    Add database connexion

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -2,23 +2,40 @@ import { Sequelize, DataTypes } from "sequelize";
 import PokemonModel from "../models/pokemon.js";
 import UserModel from "../models/user.js";
 import pokemonsData from "./mock-pokemon.js";
-import bcrypt from "bcrypt"; //encrypté le mdp
+import bcrypt from "bcrypt"; //encrypté les mdp
 let pokemons = pokemonsData;
-
+let sequelize;
 // Connexion ORM base de données
-const sequelize = new Sequelize(
-  "pokedex", // Nom de la base de données
-  "root", //Identifiant permettant d'acceder a la base de données par defaut c'est root
-  "", //Mot de passe permettant d'acceder a la base de données par defaut chaine de caractere vide
-  {
-    host: "localhost", //indique ou ce trouve la bdd sur la machine
-    dialect: "mariadb", //Nom du driver que nous utilisons pour permettre a sequelize de d'interagir avec la bdd
-    dialectOptions: {
-      timezone: "Etc/GMT-2",
-    },
-    logging: false, // Permette d'eviter l'affichage d'avertissement dans la console
-  }
-);
+// Ici on rentre les identifiants obtenue de la part de JAWSDB de la part de Heroku pour utulise la bdd de production si la variable NODE_ENV est sur Prod
+if (process.env.NODE_ENV === "production") {
+  sequelize = new Sequelize(
+    "viidybx7fxpg0pus", // Nom de la base de données
+    "r1dff0l3wmmo2k67", //Identifiant permettant d'acceder a la base de données par defaut c'est root
+    "z1c1ogr41bhhxkby", //Mot de passe permettant d'acceder a la base de données par defaut chaine de caractere vide
+    {
+      host: "iu51mf0q32fkhfpl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", //indique ou ce trouve la bdd sur la machine
+      dialect: "mariadb", //Nom du driver que nous utilisons pour permettre a sequelize de d'interagir avec la bdd
+      dialectOptions: {
+        timezone: "Etc/GMT-2",
+      },
+      logging: true, // Permette d'eviter l'affichage d'avertissement dans la console
+    }
+  );
+} else {
+  sequelize = new Sequelize(
+    "pokedex", // Nom de la base de données
+    "root", //Identifiant permettant d'acceder a la base de données par defaut c'est root
+    "", //Mot de passe permettant d'acceder a la base de données par defaut chaine de caractere vide
+    {
+      host: "localhost", //indique ou ce trouve la bdd sur la machine
+      dialect: "mariadb", //Nom du driver que nous utilisons pour permettre a sequelize de d'interagir avec la bdd
+      dialectOptions: {
+        timezone: "Etc/GMT-2",
+      },
+      logging: false, // Permette d'eviter l'affichage d'avertissement dans la console
+    }
+  );
+}
 
 //Instanciation de la fonction define pour crée la table/model
 export const Pokemon = PokemonModel(sequelize, DataTypes);
@@ -27,8 +44,9 @@ export const User = UserModel(sequelize, DataTypes);
 // Integration et synchronisation de tout nos model (sequelize* sinon si c'est seulement un model on met le nom de const qui instancie ici Pokemon.sync)
 // {force:true} ca supprime toute les table avant de synchro on laisse comme ca pendant le dev puis on enlevera
 export const initDb = () => {
-  sequelize
-    .sync({ force: true })
+  // .sync({ force: true }) force true pour le mode developement car on ecrase les donnée a chaque demarage
+  return sequelize
+    .sync()
     .then((_) => {
       console.log(`La base de données Pokedex a bien été synchronisée`);
       // Creation d'une ligne dans la table Pokemons (creation d'un pokemon)
